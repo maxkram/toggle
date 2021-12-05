101b81c213eeebc8d49e59210a0987434bdece70
maxkram <max.kramer@live.com>
2021-12-04 19:54:48 -0500

    first one

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const articles = [
+  {
+    id: 1,
+    title: 'How I spent summer',
+    date: new Date(2021, 12, 1),
+    length: 121,
+    snippet: `It was hot and sunny. Dust and dragonflies flew around`,
+  },
+  {
+    id: 2,
+    title: 'What autumn has brought',
+    date: new Date(2021, 12, 2),
+    length: 52,
+    snippet: `The dragonflies flew away, leaving their caps and shoes. They promised to return in the spring.`,
+  },
+  {
+    id: 3,
+    title: 'What are we waiting from winter?',
+    date: new Date(2021, 12, 3),
+    length: 72,
+    snippet: `New Canucks victories at the Canadiens and vice versa`,
+  },
+  {
+    id: 4,
+    title: 'What the coming spring will bring us?',
+    date: new Date(2021, 12, 4),
+    length: 75,
+    snippet: `New strains of coronavirus bein sur`,
+  },
+];
+
+export default articles;
